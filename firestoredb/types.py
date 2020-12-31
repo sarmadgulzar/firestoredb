5841feb69809f4f1fafdac437a06f31daaaa2fa2
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2021-01-01 00:01:53 +0500

    Added firestore supported data type classes

---
+++ firestoredb/types.py
@@ -0,0 +1,34 @@
+class Array:
+    pass
+
+
+class Boolean:
+    pass
+
+
+class GeoPoint:
+    pass
+
+
+class Map:
+    pass
+
+
+class Null:
+    pass
+
+
+class Number:
+    pass
+
+
+class Reference:
+    pass
+
+
+class String:
+    pass
+
+
+class Timestamp:
+    pass
