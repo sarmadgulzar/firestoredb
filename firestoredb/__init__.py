70f22d0251a1478c6b3ce655788cbd5ec2f31650
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2021-01-01 00:06:14 +0500

    Imported types classes into init file for easy access

--- firestoredb/__init__.py
+++ firestoredb/__init__.py
@@ -3,4 +3,16 @@ try:
 except ImportError:
     raise ImportError("Please ensure google-cloud-firestore version 2.0.2 is installed.")
 
+from types import (
+    Array,
+    Boolean,
+    GeoPoint,
+    Map,
+    Null,
+    Number,
+    Reference,
+    String,
+    Timestamp,
+)
+
 __version__ = '0.0.1'
